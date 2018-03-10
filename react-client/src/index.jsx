c6ca818ca54a3f95e7a4e738e6c1a48964ed05f6
acdibble <acdibble@gmail.com>
2018-03-10 10:59:55 -0600

    Rebrand

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import $ from 'jquery';  //replace with axios
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
