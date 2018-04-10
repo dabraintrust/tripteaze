3d5159ac3aab98645b85225f2f16ffdd7eccf010
acdibble <acdibble@gmail.com>
2018-04-10 17:18:11 -0500

    Rebrand

--- react-client/src/components/searchPage.jsx
+++ react-client/src/components/searchPage.jsx
@@ -630,7 +630,7 @@ class SearchPage extends React.Component {
           {/******************************* HEADER *******************************/}
           <div style={theme.styles.header}>
             <Link to="/" style={{textDecoration: 'none', color: deepOrange900}}>
-              Shahzeb&apos;s Travel Emporium
+              Tripsiesium
             </Link>
           </div>
 
