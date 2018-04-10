c9a89bfc422b9b531bcee71b9653e6e4eb0511d1
acdibble <acdibble@gmail.com>
2018-04-10 17:04:28 -0500

    Change TripDeez to Shahzeb's Travel Emporium

--- react-client/src/components/searchPage.jsx
+++ react-client/src/components/searchPage.jsx
@@ -630,7 +630,7 @@ class SearchPage extends React.Component {
           {/******************************* HEADER *******************************/}
           <div style={theme.styles.header}>
             <Link to="/" style={{textDecoration: 'none', color: deepOrange900}}>
-              TripDeez
+              Shahzeb&apos;s Travel Emporium
             </Link>
           </div>
 
